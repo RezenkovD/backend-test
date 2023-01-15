8f7f35334730df5686f16ad04d441c25b0cae1e5
RezenkovD <rezenkovdmitro@gmail.com>
2023-01-15 18:53:34 +0200

    Add type annotation and improve understanding project

--- routers.py
+++ routers.py
@@ -1,40 +1,39 @@
+import json
 import os
 
 from fastapi import APIRouter, File, UploadFile
 from pydrive2.drive import GoogleDrive
 from pydrive2.auth import GoogleAuth
 
-from crud import upload_to_google_drive
-from crud import get_file_for_key
+from utils import upload_to_google_drive, get_file_for_key
 
 router = APIRouter()
 
-folder_id = os.getenv("folder_id")
+FOLDER_ID = os.getenv("FOLDER_ID")
 
 
 @router.post("/upload-file/")
-async def upload_file(data: UploadFile = File(...)):
+def upload_file(data: UploadFile = File(...)) -> json:
     try:
         g_auth = GoogleAuth()
         drive = GoogleDrive(g_auth)
         g_auth.LocalWebserverAuth()
         upload_to_google_drive(
-            drive=drive, g_auth=g_auth, data=data, folder_id=folder_id
+            drive=drive, g_auth=g_auth, data=data, folder_id=FOLDER_ID
         )
-        return {"Success": True}
+        return {"Status": True}
 
-    except Exception as ex:
-        print("Error: ", str(ex))
+    except Exception:
         return {"Success": False}
 
 
 @router.get("/get-file/")
-async def get_file(key: str):
+def get_file(key: str) -> json:
     try:
         g_auth = GoogleAuth()
         drive = GoogleDrive(g_auth)
-        get_file_for_key(drive=drive, key=key, folder_id=folder_id)
-        return {"Success": True}
+        output = get_file_for_key(drive=drive, key=key, folder_id=FOLDER_ID)
+        return {"Success": True, "file_name": output}
 
     except Exception as ex:
         print("Error: ", str(ex))
--- utils.py
+++ utils.py
@@ -2,19 +2,24 @@ import json
 import requests
 
 from fastapi import UploadFile
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
 
 
-def search_file_in_drive(drive, key, folder_id: str):
+def search_file_in_drive_or_none(
+    drive: GoogleDrive, key: str, folder_id: str
+) -> GoogleDrive:
     file_list = drive.ListFile(
         {"q": f"'{folder_id}' in parents and trashed=false"}
     ).GetList()
     for file in file_list:
         if key == file["title"]:
             return file
-    return None
 
 
-def upload_to_google_drive(drive, g_auth, data: UploadFile, folder_id: str):
+def upload_to_google_drive(
+    drive: GoogleDrive, g_auth: GoogleAuth, data: UploadFile, folder_id: str
+) -> int:
     key = data.filename
     value = data.file
     metadata = {
@@ -25,23 +30,26 @@ def upload_to_google_drive(drive, g_auth, data: UploadFile, folder_id: str):
         "data": ("metadata", json.dumps(metadata), "application/json"),
         "file": value,
     }
-    file = search_file_in_drive(drive, key, folder_id)
+    file = search_file_in_drive_or_none(drive=drive, key=key, folder_id=folder_id)
     if file is not None:
         file_id = f"{file['id']}"
-        requests.put(
+        response = requests.put(
             f"https://www.googleapis.com/upload/drive/v2/files/{file_id}?uploadType=multipart",
             headers={"Authorization": "Bearer " + g_auth.credentials.access_token},
             files=files,
         )
+        return response.status_code
     else:
-        requests.post(
+        response = requests.post(
             "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",
             headers={"Authorization": "Bearer " + g_auth.credentials.access_token},
             files=files,
         )
+        return response.status_code
 
 
-def get_file_for_key(drive, key, folder_id: str):
-    file = search_file_in_drive(drive, key, folder_id)
+def get_file_for_key(drive: GoogleDrive, key: str, folder_id: str) -> str:
+    file = search_file_in_drive_or_none(drive=drive, key=key, folder_id=folder_id)
     if file is not None:
         file.GetContentFile(file["title"])
+        return file["title"]
